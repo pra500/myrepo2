60f1d89959056236db72475559b18d56e2293b7a
pra500 <pragtig6@gmail.com>
2020-06-20 12:51:56 +0530

    new changes

---
+++ src/main/java/com/qa/pages/MyContactspage.java
@@ -0,0 +1,10 @@
+package com.qa.pages;
+
+public class MyContactspage {
+	
+	public void contactpage1()
+	{
+		System.out.println("this is contactpage1 method");
+	}
+
+}
--- src/main/java/com/qa/util/MyUtility.java
+++ src/main/java/com/qa/util/MyUtility.java
@@ -5,6 +5,8 @@ public class MyUtility {
 	public void MyUtility1()
 	{
 		System.out.println("this is MyUtility1 method");
+		
+		System.out.println("this is MyUtility2 method");
 	}
 	
 
