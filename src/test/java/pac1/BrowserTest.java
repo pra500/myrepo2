a822bb67262945229363ab7f302986ce02c950e5
pra500 <pragtig6@gmail.com>
2020-08-06 17:49:24 +0530

    author name

---
+++ src/test/java/pac1/BrowserTest.java
@@ -0,0 +1,18 @@
+package pac1;
+
+import org.testng.annotations.Test;
+
+public class BrowserTest {
+	
+	/*
+	 * 
+	 * @author pragti gupta
+	 */
+	
+	
+  @Test
+  public void fTest() {
+	  
+	  System.out.println("hi");
+  }
+}
